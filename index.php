611c3578644b8d248818e1ae11ae390e81cbc4b3
erskin19 <tyler.w.erskine@gmail.com>
2016-04-14 13:38:50 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+require 'format.inc.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Brittany Hackel</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+<?php echo present_header(); ?>
+<section>
+    <form method="post" action="game-post.php">
+        <h2>Please enter name</h2>
+        <p class="label">Name:
+            <label for="name" ><input type="text" name="name"
+                                      id="name"></p>
+        <p class="submit"><input type="submit" value="Start Game"></p>
+    </form>
+</section>
+
+<section>
+    <a href="game-post.php?cheat">
+        <div class="giveUp">
+            <h2>Cheat Mode</h2>
+        </div>
+    </a>
+</section>
+</body>
+
+</html>
