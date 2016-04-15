48a02a297ad5811858fc8b09fa0436f223606f00
erskin19 <tyler.w.erskine@gmail.com>
2016-04-15 03:16:56 -0400

    portfolio blocks css

--- portfolio.php
+++ portfolio.php
@@ -9,7 +9,11 @@ require 'lib/site.inc.php';
 
 <body>
 <?php echo present_header(); ?>
-
+<div class="row">
+    <div class="col-12 col-m-12 col-t-12">
+        <h2>Portfolio</h2>
+    </div>
+</div>
 <div id="projects" class="row">
     <div class="col-3 col-t-4 col-m-12"><img src="img/placeholder.png" alt="placholder" /></div>
     <div class="col-3 col-t-4 col-m-12"><img src="img/placeholder.png" alt="placholder" /></div>
@@ -19,8 +23,6 @@ require 'lib/site.inc.php';
     <div class="col-3 col-t-4 col-m-12"><img src="img/placeholder.png" alt="placholder" /></div>
     <div class="col-3 col-t-4 col-m-12"><img src="img/placeholder.png" alt="placholder" /></div>
     <div class="col-3 col-t-4 col-m-12"><img src="img/placeholder.png" alt="placholder" /></div>
-
-
 </div>
 </body>
 
