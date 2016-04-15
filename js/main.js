0f46adf7ea8c520befc93297b6e942b8421cc9d8
erskin19 <tyler.w.erskine@gmail.com>
2016-04-15 14:13:28 -0400

    portfolio popup js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 /**
  * Created by Tyler on 4/15/2016.
  */
+$('.project').click(function() {
+    console.log(this);
+});
--- portfolio.php
+++ portfolio.php
@@ -15,7 +15,7 @@ require 'lib/site.inc.php';
     </div>
 </div>
 <div id="projects" class="row">
-    <div class="col-3 col-t-4 col-m-12"><img src="img/placeholder.png" alt="placholder" /></div>
+    <div class="col-3 col-t-4 col-m-12"><a class="project" id="1"><img src="img/placeholder.png" alt="placholder" /></a></div>
     <div class="col-3 col-t-4 col-m-12"><img src="img/placeholder.png" alt="placholder" /></div>
     <div class="col-3 col-t-4 col-m-12"><img src="img/placeholder.png" alt="placholder" /></div>
     <div class="col-3 col-t-4 col-m-12"><img src="img/placeholder.png" alt="placholder" /></div>
