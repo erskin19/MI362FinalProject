25e3381687da15f2dc14a34a540652343b376686
erskin19 <tyler.w.erskine@gmail.com>
2016-04-15 14:50:12 -0400

    portfolio popup js

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
     console.log( "ready!" );
-    $('.project').click(projectClick(this));
+    $('.project').click(projectClick(this.id));
 });
 
 // function called when a project is clicked on
-function projectClick(object){
-    console.log(this.id);
+function projectClick(id){
+    console.log(id);
 }
