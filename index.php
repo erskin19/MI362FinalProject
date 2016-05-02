e249b5401cdb7b24fcace2019854212cb9ed0f7f
erskin19 <tyler.w.erskine@gmail.com>
2016-05-02 16:37:19 -0400

    added about me contact

--- index.php
+++ index.php
@@ -12,11 +12,90 @@ require 'lib/site.inc.php';
 
 <div class="row" id="about">
     <div class="col-1 col-t-1 col-m-1"></div>
-    <div class="col-3 col-t-3 col-m-10"><img src="img/placeholder.png" alt="placholder" /></div>
+    <div class="col-3 col-t-3 col-m-10"><img src="img/brittany_hackel.JPG" alt="Brittany Hackel" /></div>
     <div class="col-1 col-t-1 col-m-1"></div>
     <div class="col-6 col-t-6 col-m-12">
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
+        <p>I always have had an interest in the arts, but never thought it would be a career path.
+
+            Throughout high school, I had taken a few art and architecture classes to stimulate my interest
+
+            as more of a hobby.  Using art as an outlet, I fell in love with the sciences.  This love stemmed
+
+            from my senior year Anatomy and Physiology course and stimulated my decision that I wanted
+
+            to pursue a career in the sciences and help people and animals.</p>
+        <p>At the start of my college career, I indulged in a full course load of classes that guided
+
+            me on a science path, but still undecided on the route I wanted to take.  Taking the prerequisite
+
+            classes for a Natural Science major of my freshman and sophomore year, I slowly started to
+
+            struggle and question my decisions. This struggle made me realize I needed to research other
+
+            career paths that allowed me to help people or animals.  Taking a career test and talking to
+
+            advisors, I explored the careers that most interested me.  After the research, I proceeded with
+
+            caution on a focus in interior design.  Having an interest, I was recommended to read an article
+
+            about designing for the wellness of individuals.  This same process of design was present in a
+
+            crisis resource center through Common Ground, a crisis intervention agency, at an event where
+
+            I was a volunteer.   I was taught that the building was designed specifically to help the people
+
+            the center serves who have cases of crisis and or mental illness.  The design of the building had
+
+            many curvilinear lines present and a neutral color scheme with accents of blues and greens to
+
+            provide a more calming and stress free environment.  Designing for the wellness of individuals
+
+            is scientific in a way with respect of the concern for their health.</p>
+        <p>This scientific background excited me and made me realize I could have a career with
+
+            both sciences and arts.  But still being skeptical, I didn’t want to jump in a different major too
+
+            quick.  To see if I wanted to pursue a different major or continue on my science route, I enrolled
+
+            in a study abroad to Kenya through the Zoology program and an online Interior Design course
+
+            during the summer.  As much as I loved science, animals, and my trip to Kenya, Zoology or the
+
+            science route was not a fit for me.  I was not as excited as I should be when taking the science
+
+            courses as with when I participated in the Interior design online course.  I was ready to fully
+
+            indulge in a new major because I was excited to learn and identify the elements and principles
+
+            of design and how to design a functional space to benefit an individual.
         </p>
+        <p>I felt like I found my true calling and passion in interior design.  Before I was accepted
+
+            into the interior design upper division, I was able to obtain an internship for that spring semester
+
+            and summer.  I had at my internship with DBI Business Interiors, a Haworth dealer that designs
+
+            office spaces, for over a year..  Through this experience, I have gained a lot of knowledge that
+
+            has helped me with many projects for courses and has given me networking opportunities.
+
+            Words cannot even describe the excitement when I was able to put my name on my first
+
+            AutoCAD drawing file that I had drawn for a designer.  Working for this company made me
+
+            realize that I love working with commercial interiors because of the ability to provide healthier
+
+            design solutions to a larger group of people.  In the future or after I graduate I would like to
+
+            design commercial spaces in healthcare, hospitality, or office spaces that will help me with
+
+            receiving my NCIDQ.  I also have an interest in ergonomics and would be willing to attend
+
+            graduate school to become specialized to design better working situations for people in
+
+            everyday life.  The opportunities in this field are limitless in regards to numerous job prospects
+
+            that are available.  You really can do what you love the most and help people at the same time.</p>
     </div>
     <div class="col-1 col-t-1 col-m-12"></div>
 </div>
