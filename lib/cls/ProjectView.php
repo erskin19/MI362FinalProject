c972153fa59d5bb73f69de4f9c6990a86e68741c
erskin19 <tyler.w.erskine@gmail.com>
2016-05-03 17:42:50 -0400

    trying to fix project view

--- lib/cls/ProjectView.php
+++ lib/cls/ProjectView.php
@@ -17,9 +17,10 @@ class ProjectView
     public function displayProjects()
     {
         $projectList = $this->projects->getAll();
+        $projectList[0]->getId();
         $html = "";
         for ($i = 0; $i < count($projectList); $i++){
-//            $html .= '<div class="col-3 col-t-4 col-m-12"><a class="project" id="'. $projectList[$i]->getId()
+//            $html .= '<div class="col-3 col-t-4 col-m-12"><a class="project" id="'. $projectList[0]->getId()
 //                .'" href="#popup"><img src="img/'.$projectList[$i]->getCover()
 //                .'" alt="'.$projectList[$i]->getTitle().'" /></a></div>';
         }
