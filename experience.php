25e85b748520a7f1724540e5dbe1fce43d781697
erskin19 <tyler.w.erskine@gmail.com>
2016-05-03 17:57:58 -0400

    fixing tabs

--- experience.php
+++ experience.php
@@ -5,7 +5,7 @@ require 'lib/site.inc.php';
 <!DOCTYPE html>
 <html lang="en">
 
-<?php echo present_head("Experience"); ?>
+<?php echo present_head("Resume"); ?>
 
 <body>
 <?php echo present_header(); ?>
