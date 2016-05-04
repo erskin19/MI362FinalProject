1e3ac10e31b0e7df2dbf58dedb0c2ea3b46f5a2c
erskin19 <tyler.w.erskine@gmail.com>
2016-05-03 23:10:08 -0400

    embeding pdf

--- portfolio.php
+++ portfolio.php
@@ -27,9 +27,7 @@ $view = new ProjectView($site);
     <?php echo $view->displayProjects(); ?>
 </div>
 <div id="popup">
-    <a id="exitPopup" href=""> X </a>
-    <a id="lastPic" href=""> <i class="fa fa-chevron-left fa-2x"></i> </a>
-    <a id="nextPic" href=""> <i class="fa fa-chevron-right fa-2x"></i> </a>
+    <a id="exitPopup" href="">X</a>
     <object id="pdf" data="pdf/Boards.pdf" type="application/pdf" >
         <a href="pdf/Boards.pdf">Project 1</a>
     </object>
