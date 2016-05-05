16da83333ccdbdd66ca5e0107522bb588448b714
erskin19 <tyler.w.erskine@gmail.com>
2016-05-04 20:35:23 -0400

    contact me

--- contact.php
+++ contact.php
@@ -9,8 +9,22 @@ require 'lib/site.inc.php';
 
 <body>
 <?php echo present_header(); ?>
-<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
-<script type="IN/MemberProfile" data-id="https://www.linkedin.com/in/brittany-hackel-70420ba1" data-format="hover" data-text="Brittany Hackel"></script>
+    <div>
+        <h2>Contact Info</h2>
+        <h3>Email: brihackel@gmail.com</h3>
+        <a href="mailto:brihackel@gmail.com">
+            <i class="fa fa-envelope fa-3x email" aria-hidden="true"></i>
+        </a>
+        <h3>Phone: (248)881-7620</h3>
+        <a href="tel:1-248-881-7620">
+            <i class="fa fa-phone-square fa-3x phone" aria-hidden="true"></i>
+        </a>
+        <h3>Linkedin</h3>
+        <a href="https://www.linkedin.com/in/brittany-hackel-70420ba1">
+            <i class="fa fa-linkedin-square fa-3x linkedin" aria-hidden="true"></i>
+        </a>
+
+    </div>
 </body>
 
 </html>
