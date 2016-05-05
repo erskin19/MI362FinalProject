344d1491a26bab2000f7e21d95b3970a08d55fc9
erskin19 <tyler.w.erskine@gmail.com>
2016-05-04 21:07:30 -0400

    contact me

--- lib/format.inc.php
+++ lib/format.inc.php
@@ -40,9 +40,9 @@ function present_header() {
 <h1>$title</h1>
 </header>
 <div class="row" id="tabs">
-<a href="index.php#about"><div class="col-3 col-t-3 col-m-12 tabs">About Me</div></a>
-<a href="portfolio.php#projects"><div class="col-3 col-t-3 col-m-12 tabs">Portfolio</div></a>
-<a href="experience.php#experience"><div class="col-3 col-t-3 col-m-12 tabs">Resume</div></a>
+<a href="index.php"><div class="col-3 col-t-3 col-m-12 tabs">About Me</div></a>
+<a href="portfolio.php"><div class="col-3 col-t-3 col-m-12 tabs">Portfolio</div></a>
+<a href="experience.php"><div class="col-3 col-t-3 col-m-12 tabs">Resume</div></a>
 <a href="contact.php"><div class="col-3 col-t-3 col-m-12 tabs">Contact Me</div></a>
 </div>
 
