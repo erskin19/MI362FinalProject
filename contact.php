a2141e52de672dc1b17d0e565fac3f119e0ced2d
erskin19 <tyler.w.erskine@gmail.com>
2016-05-04 20:48:35 -0400

    contact me linkedin

--- contact.php
+++ contact.php
@@ -9,13 +9,13 @@ require 'lib/site.inc.php';
 
 <body>
 <?php echo present_header(); ?>
-    <div>
+    <div id="contact">
         <h2>Contact Info</h2>
         <h3>Email: brihackel@gmail.com</h3>
         <a href="mailto:brihackel@gmail.com">
             <i class="fa fa-envelope fa-3x email" aria-hidden="true"></i>
         </a>
-        <h3>Phone: (248)881-7620</h3>
+        <h3>Phone: (248) 881 - 7620</h3>
         <a href="tel:1-248-881-7620">
             <i class="fa fa-phone-square fa-3x phone" aria-hidden="true"></i>
         </a>
