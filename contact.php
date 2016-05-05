26fe6005e09702aa1d51477807601b5cc19db25d
erskin19 <tyler.w.erskine@gmail.com>
2016-05-04 20:56:52 -0400

    contact me

--- contact.php
+++ contact.php
@@ -9,19 +9,19 @@ require 'lib/site.inc.php';
 
 <body>
 <?php echo present_header(); ?>
-    <div id="contact">
+    <div id="contact" class="row">
         <h2>Contact Info</h2>
         <h3>Email: brihackel@gmail.com</h3>
         <a href="mailto:brihackel@gmail.com">
-            <i class="fa fa-envelope fa-3x email" aria-hidden="true"></i>
+            <i class="fa fa-envelope fa-5x email" aria-hidden="true"></i>
         </a>
         <h3>Phone: (248) 881 - 7620</h3>
         <a href="tel:1-248-881-7620">
-            <i class="fa fa-phone-square fa-3x phone" aria-hidden="true"></i>
+            <i class="fa fa-phone-square fa-5x phone" aria-hidden="true"></i>
         </a>
         <h3>Linkedin</h3>
         <a href="https://www.linkedin.com/in/brittany-hackel-70420ba1">
-            <i class="fa fa-linkedin-square fa-3x linkedin" aria-hidden="true"></i>
+            <i class="fa fa-linkedin-square fa-5x linkedin" aria-hidden="true"></i>
         </a>
 
     </div>
