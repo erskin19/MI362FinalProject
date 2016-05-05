b807845004baff8da5fc47e37ae681bedd362605
erskin19 <tyler.w.erskine@gmail.com>
2016-05-04 20:16:00 -0400

    contact me linkedin

--- contact.php
+++ contact.php
@@ -9,7 +9,8 @@ require 'lib/site.inc.php';
 
 <body>
 <?php echo present_header(); ?>
-
+<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+<script type="IN/MemberProfile" data-id="https://www.linkedin.com/in/brittany-hackel-70420ba1" data-format="hover" data-text="Brittany Hackel"></script>
 </body>
 
 </html>
